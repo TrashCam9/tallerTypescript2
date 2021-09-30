f86d98297df3b80ac157b48bae4b90ed4af98b39
TrashCam9 <62612964+TrashCam9@users.noreply.github.com>
2021-09-29 21:27:41 -0500

    Parte 1 Taller TypeScript

---
+++ scripts/dataCourses.js
@@ -0,0 +1,9 @@
+import { Course } from './course.js';
+export var dataCourses = [
+    new Course("Desarrollo de SW en equipo", "Cesar Forero", 3),
+    new Course("TI en las organizaciones", "Cindy Otalora", 3),
+    new Course("Diseño y Analisis de Algoritmos", "Ruben Manrique", 3),
+    new Course("Calculo Vectorial", "Jairo Angel", 3),
+    new Course("Sistemas Transaccionales", "German Bravo", 3),
+    new Course("Biología Celular", "Jesus Uribe", 3)
+];
